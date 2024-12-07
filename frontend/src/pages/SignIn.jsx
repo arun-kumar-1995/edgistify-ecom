c85d8904bd78f9aad8753e49c7962766d569ec03
arun kumar <arun.devpro@gmail.com>
2024-12-07 11:58:15 +0530

    sign in and sign up markup

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,10 +1,64 @@
+import { useEffect, useState, useCallback } from "react";
+import "../styles/form.css";
+import logo from "/assets/logo.jpg";
+import { Link } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa6";
+import { MdLockOutline } from "react-icons/md";
+
 const SignIn = () => {
-    return (
-      <div className="SignIn">
-        <h1>SignIn</h1>
-      </div>
-    );
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+
+  const handleLoginInput = useCallback((e) => {
+    const { name, value } = e.target;
+    setLoginInput((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
   };
-  
-  export default SignIn;
-  
+
+  return (
+    <div className="login-container form-container">
+      <div className="form-wrapper">
+        <form onSubmit={handleLoginSubmit}>
+          <div className="form-info">
+            <h2>Sign In !</h2>
+            <p>
+              Are you new here?
+              <Link to="/sign-up">SignUp</Link>
+            </p>
+          </div>
+
+          <div className="form-section">
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                placeholder="Enter username"
+                value={loginInput.email}
+                name="email"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+
+              <input
+                type="password"
+                placeholder="Enter password"
+                value={loginInput.password}
+                name="password"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,87 @@
+import { useEffect, useState, useCallback } from "react";
+import "../styles/form.css";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-    return (
-      <div className="home">
-        <h1>SignUp</h1>
-      </div>
-    );
+  const [signUpInput, setSignUpInput] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleLoginInput = useCallback((e) => {
+    const { name, value } = e.target;
+    setSignUpInput((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
   };
-  
-  export default SignUp;
-  
+
+  return (
+    <div className="signup-container form-container">
+      <div className="form-wrapper">
+        <form onSubmit={handleLoginSubmit}>
+          <div className="form-info">
+            <h2>Sign Up !</h2>
+            <p>
+              Already have an account?
+              <Link to="/sign-in">SignIn</Link>
+            </p>
+          </div>
+
+          <div className="form-section">
+            <div className="form-group">
+              <label htmlFor="fullName">Enter name</label>
+              <input
+                type="text"
+                placeholder="First and last name"
+                value={signUpInput.fullName}
+                name="fullName"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Enter email</label>
+              <input
+                type="text"
+                placeholder="sampleemail@gmail.com"
+                value={signUpInput.email}
+                name="email"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="min 8 character"
+                value={signUpInput.password}
+                name="password"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Re-enter Password</label>
+              <input
+                type="password"
+                placeholder=""
+                value={signUpInput.confirmPassword}
+                name="confirmPassword"
+                onChange={handleLoginInput}
+                required
+              />
+            </div>
+            <button type="submit">Create account</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
